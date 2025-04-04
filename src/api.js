cfda80921e6f06a58ea97d146cacc16370e7e62d
Asadbek Rakhmonjonov <rakhmonjonovasadbek8@gmail.com>
2025-04-04 19:13:48 +0900

    Switch to free GPT-2 model for recipe suggestions

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export async function getRecipeSuggestions(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ");
     try {
         const response = await hf.chatCompletion({
-            model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            model: "gpt2", 
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
